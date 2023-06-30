c65d6388b1656277b294228fb8665ee45a0afa72
tlschdaud <119427608+tlschdaud@users.noreply.github.com>
2023-06-30 12:36:55 +0900

    Add files via upload

---
+++ 4. 프로그래밍 언어응용/java_study/day01/day07/src/exam02/Ex05.java
@@ -0,0 +1,12 @@
+package exam02;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        A a = new A() {
+          public void method() {
+              System.out.println("(수정)A메서드");
+          }
+        };
+        a.method();
+    }
+}
