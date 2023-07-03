46ff49287f92470fe5962e8818a7657d0f018759
tlschdaud <119427608+tlschdaud@users.noreply.github.com>
2023-07-03 12:52:20 +0900

    Add files via upload

---
+++ 4. 프로그래밍 언어응용/java_study/day01/day08/src/exam02/Ex01.java
@@ -0,0 +1,24 @@
+package exam02;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream("a.txt");
+
+            fis.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                fis.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
---
+++ 4. 프로그래밍 언어응용/java_study/day01/day08/src/exam02/Ex02.java
@@ -0,0 +1,28 @@
+package exam02;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream("a.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("항상");
+            if( fis != null) {
+            try {
+                fis.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+
+            }
+
+        }
+    }
+}
