758d87927781061a4c0f74db519d4dd4c7f51f4b
“tlschdaud” <tlschdaud1@naver.com>
2023-08-23 14:22:08 +0900

    test

--- 7.통합구현/springboot_study/exam02/src/main/java/org/koreait/constants/UserType.java
+++
@@ -1,6 +0,0 @@
-package org.koreait.constants;
-
-public enum UserType {
-    ADMIN, // 관리자
-    USER // 일반 회원
-}
--- 7.통합구현/springboot_study/exam02/src/main/java/org/koreait/repositories/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package org.koreait.repositories;
-
-import org.koreait.entities.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
